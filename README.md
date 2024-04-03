04a922fd8b3def42f940efb62f3b332d4dc88019
Alexander Forsanker <alexanderforsanker@wlan13864.eduroam.hj.se>
2024-04-03 17:43:28 +0200

    Created the base project structure for the API

---
+++ README.md
@@ -0,0 +1,51 @@
+# Backend Project
+
+Welcome to the hvsc-g7 backend! This repository contains the code for the backend of our application.
+
+## Installation
+
+To get started, follow these steps:
+1. Clone this repository to your local machine.
+2. Navigate to the project directorys root: `your-directory-path/hvsc-g7-backend/`
+3. Install the project dependencies by running: `pip install -r requirements.txt`.
+
+## Usage
+Once you have installed the project dependencies, you can run the backend using the following command: 
+
+## Project frameworks description:
+This code utilizes several frameworks for web development and API specification. Here's a brief overview of each framework and its purpose:
+
+1. Flask:
+    - Flask is a lightweight web framework for Python.
+    - It provides a simple and flexible way to build web applications.
+    - Flask is commonly used for developing RESTful APIs and web services.
+
+2. Requests:
+    - Requests is a Python library for making HTTP requests.
+    - It simplifies the process of sending HTTP requests and handling responses.
+    - Requests is often used for interacting with external APIs or making HTTP calls to other web services.
+
+3. apispec:
+    - apispec is a Python library for building and documenting APIs.
+    - It provides a way to define API schemas and generate API documentation.
+    - apispec is commonly used in combination with web frameworks like Flask to document API endpoints and request/response models.
+
+4. apispec-webtrameworks:
+    - apispec-webtrameworks is an extension for apispec that adds support for web frameworks.
+    - It provides integration with popular web frameworks like Flask, allowing for automatic generation of API documentation.
+
+5. Marshmallow:
+    - Marshmallow is a Python library for object serialization and deserialization.
+    - It provides a simple way to convert complex data types, such as objects or database records, to and from JSON or other formats.
+    - Marshmallow is often used in combination with web frameworks and APIspec to validate and serialize/deserialize data in API requests and responses.
+
+6. PyTest:
+    - PyTest is a testing framework for Python.
+    - It provides a simple and scalable approach to writing tests.
+    - PyTest supports fixtures, parameterization, and plugins, making it versatile for various testing scenarios.
+
+7. assertpy:
+    - assertpy is an assertion library for Python.
+    - assertpy's API allows for chaining of assertions and better error messages, for clarity in test cases.
+
+These frameworks are widely used in the Python ecosystem for developing web applications, building APIs, and documenting APIs. They provide a range of features and functionalities that simplify the development process and improve the overall quality of web applications.
---
+++ app.py
@@ -0,0 +1,124 @@
+from apispec import APISpec
+from flask import Flask, jsonify, render_template, send_from_directory, redirect, url_for
+from apispec_webframeworks.flask import FlaskPlugin
+from apispec.ext.marshmallow import MarshmallowPlugin
+from marshmallow import Schema, fields
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__, template_folder='./swagger/templates')
+
+@app.route('/')
+def index():
+    """Redirect to Swagger documentation"""
+    return redirect(url_for('swagger_docs'))
+
+spec = APISpec(
+    title='HVSC-G7-Backend',
+    version='1.0.0',
+    openapi_version='3.0.2',
+    plugins=[FlaskPlugin(), MarshmallowPlugin()],
+)
+
+@app.route('/api/swagger.json')
+def create_swagger_spec():
+    return jsonify(spec.to_dict())
+
+class PositionSchema(Schema):
+    longitude = fields.Float()
+    latitude = fields.Float()
+    timestamp = fields.DateTime()
+
+class MoverResponseSchema(Schema):
+    moverId = fields.Int()
+    positions = fields.List(fields.Nested(PositionSchema))
+
+class TodoResponseSchema(Schema):
+    id = fields.Int()
+    title = fields.Str()
+    status = fields.Boolean()
+
+class TodoListResponseSchema(Schema):
+    todo_list = fields.List(fields.Nested(TodoResponseSchema))
+
+
+@app.route('/mover/<int:moverId>')
+def mover(moverId):
+    """Get the information of a single mover by moverId
+    ---
+    get:
+        description: Get the information of a single mover by moverId
+        parameters:
+          - in: path
+            name: moverId
+            schema:
+              type: integer
+            required: true
+            description: ID of the mover
+        responses:
+            200:
+                description: Return the information of a mover by moverId
+                content:
+                    application/json:
+                        schema: MoverResponseSchema
+    """
+
+    dummy_data = {
+        'moverId': moverId,
+        'positions': [
+            {
+                'longitude': 53.13,
+                'latitude': 109.09,
+                'timestamp': '2021-07-01T00:00:00Z'
+            }, 
+            {
+                'longitude': 53.15,
+                'latitude': 109.10,
+                'timestamp': '2021-07-01T00:01:00Z'
+            }
+        ]
+    }
+
+    return jsonify(dummy_data)
+
+
+@app.route('/todo')
+def todo():
+    """Get List of Todo
+    ---
+    get:
+        description: Get List of Todos
+        responses:
+            200:
+                description: Return a todo list
+                content:
+                    application/json:
+                        schema: TodoListResponseSchema
+    """
+
+    dummy_data = [{
+        'id': 1,
+        'title': 'Finish this task',
+        'status': False
+    }, {
+        'id': 2,
+        'title': 'Finish that task',
+        'status': True
+    }]
+
+    return TodoListResponseSchema().dump({'todo_list': dummy_data})
+
+
+with app.test_request_context():
+    spec.path(view=todo)
+    spec.path(view=mover)
+
+@app.route('/docs')
+@app.route('/docs/<path:path>')
+def swagger_docs(path=None):
+    if not path or path == 'index.html':
+        return render_template('index.html', base_url='/docs')
+    else:
+        return send_from_directory('./swagger/static', secure_filename(path))
+
+if __name__ == '__main__':
+    app.run(debug=True)
